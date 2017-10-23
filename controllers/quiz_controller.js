0a7b238bdd07e412dae4de4f658b25f693fbb56e
jotamontoyo <jotamontoyo@gmail.com>
2017-10-23 16:49:05 +0100

    correccion options index lecturas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -43,12 +43,13 @@
 	// GET /users/:userId/quizes							--->>> GET con req.user
 	exports.index = function(req, res, next) {
 
+
 		var options = {
-			where: {mes: mes, anio: anio},
+			where: {centro: req.session.user.centro, mes: mes, anio: anio},
 			order: [['fecha', 'ASC']]
 		};
 
-	  	if (req.user) {									// req.user se crea en autoload de user_controller si hay un GET con un user logueado
+		if (req.user) {									// req.user se crea en autoload de user_controller si hay un GET con un user logueado.
 			options = {
 				where: {UserId: req.user.id, centro: req.session.user.centro, mes: mes, anio: anio},
 				order: [['fecha', 'ASC']]
